481f6e35f062d5c60554253bd778f296cb3d0381
Leandro <leandro@leandro.com.br>
2018-01-25 11:59:41 -0200

    colocando o gerenciador de testes na controller

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
 public class SessaoController {
@@ -28,17 +31,7 @@ public class SessaoController {
 	private FilmeDao filmeDao;
 	
 	
-	
-	public ModelAndView form(@RequestParam("salaId") Integer salaId) {
-		
-		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
-		
-		modelAndView.addObject("sala", salaDao.findOne(salaId));
-		modelAndView.addObject("filmes", filmeDao.findAll());
-		
-		return modelAndView;
-		
-	}
+
 	
 	@GetMapping("/admin/sessao")
 	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
@@ -61,12 +54,14 @@ public class SessaoController {
 		
 		if (result.hasErrors()) return form(form.getSalaId(),form);
 		
-		ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala/"+form.getSalaId()+"/sessoes");
+		Sessao	sessao	=	form.toSessao(salaDao,	filmeDao);
 		
-		Sessao sessao = form.toSessao(salaDao, filmeDao);
-		
-		sessaoDao.save(sessao);
-		
-		return modelAndView;
+		List<Sessao>	sessoesDaSala	=	sessaoDao.buscaSessoesDaSala(sessao.getSala());
+		GerenciadorDeSessao	gerenciador	=	new	GerenciadorDeSessao(sessoesDaSala);
+		if	(gerenciador.cabe(sessao))	{
+						sessaoDao.save(sessao);
+						return	new	ModelAndView("redirect:/admin/sala/"	+	form.getSalaId()	+	"/sessoes");
+		}
+		return	form(form.getSalaId(),	form);
 	}
 }
