d0b5dd396980a935c1adc3efc38e2577479bd54a
Leandro <leandro@leandro.com.br>
2018-01-26 22:47:23 -0200

    finalizado o exec4.4

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -2,8 +2,10 @@ package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.ImdbClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,6 +30,8 @@ public class FilmeController {
     private FilmeDao filmeDao;
     @Autowired
     private SessaoDao sessaoDao;
+    @Autowired
+    private ImdbClient client;
 
 
     @GetMapping({"/admin/filme", "/admin/filme/{id}"})
@@ -85,17 +89,16 @@ public	ModelAndView	emCartaz(){
         filmeDao.delete(id);
     }
     
-    @GetMapping("/filme/{id}/detahle")
-    public ModelAndView detalhes(@PathVariable("id") Integer id){
-    	ModelAndView modelAndView = new ModelAndView("filme/detalhe");
-    	
-    	Filme filme = filmeDao.findOne(id);
-    	List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
-    	
-    	modelAndView.addObject("sessoes",sessoes);
-    	
-    	return modelAndView;
-    }
+	@GetMapping("/filme/{id}/detalhe")
+	public	ModelAndView	detalhes(@PathVariable("id")	Integer	id){
+					ModelAndView	modelAndView	=	new	ModelAndView("/filme/detalhe");
+					Filme	filme	=	filmeDao.findOne(id);
+					List<Sessao>	sessoes	=	sessaoDao.buscaSessoesDoFilme(filme);
+					Optional<DetalhesDoFilme>	detalhesDoFilme	=	client.request(filme);
+					modelAndView.addObject("sessoes",	sessoes);
+					modelAndView.addObject("detalhes",	detalhesDoFilme.orElse(new	DetalhesDoFilme()));
+					return	modelAndView;
+	}
     
     
 
